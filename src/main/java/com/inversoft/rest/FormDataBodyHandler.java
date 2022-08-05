7065d17f1ce71f46ba95ecb7d90647817476e5f5
Daniel DeGroff <daniel@fusionauth.io>
2022-08-05 13:03:14 -0600

    squash merge degroff/netty_shrink

--- src/main/java/com/inversoft/rest/FormDataBodyHandler.java
+++ src/main/java/com/inversoft/rest/FormDataBodyHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2021, Inversoft Inc., All Rights Reserved
+ * Copyright (c) 2016-2022, Inversoft Inc., All Rights Reserved
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,9 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -39,6 +42,10 @@ public class FormDataBodyHandler implements RESTClient.BodyHandler {
     this.excludeNullValues = excludeNullValues;
   }
 
+  public FormDataBodyHandler() {
+    request = new HashMap<>();
+  }
+
   public FormDataBodyHandler(Map<String, List<String>> request) {
     this.request = request;
   }
@@ -48,6 +55,26 @@ public class FormDataBodyHandler implements RESTClient.BodyHandler {
     if (body != null && os != null) {
       os.write(body);
     }
+  }
+
+  public FormDataBodyHandler addParameter(String key, List<String> value) {
+    if (value == null) {
+      request.put(key, null);
+    } else {
+      request.computeIfAbsent(key, k -> new ArrayList<>(1)).addAll(value);
+    }
+
+    return this;
+  }
+
+  public FormDataBodyHandler addParameter(String key, String value) {
+    if (value == null) {
+      request.put(key, null);
+    } else {
+      request.computeIfAbsent(key, k -> new ArrayList<>(1)).add(value);
+    }
+
+    return this;
   }
 
   @Override
@@ -74,6 +101,33 @@ public class FormDataBodyHandler implements RESTClient.BodyHandler {
       huc.addRequestProperty("Content-Type", "application/x-www-form-urlencoded");
       huc.addRequestProperty("Content-Length", "" + body.length);
     }
+  }
+
+  public FormDataBodyHandler withExcludeNullValues(boolean excludeNullValues) {
+    this.excludeNullValues = excludeNullValues;
+    return this;
+  }
+
+  public FormDataBodyHandler withParameter(String key, List<String> value) {
+    request.put(key, value);
+    return this;
+  }
+
+  public FormDataBodyHandler withParameter(String key, String value) {
+    if (value == null) {
+      request.put(key, null);
+    } else {
+      //noinspection ArraysAsListWithZeroOrOneArgument
+      request.put(key, Arrays.asList((value)));
+    }
+
+    return this;
+  }
+
+  public FormDataBodyHandler withParameters(Map<String, String> parameters) {
+    //noinspection ArraysAsListWithZeroOrOneArgument
+    parameters.keySet().forEach(k -> request.put(k, Arrays.asList(parameters.get(k))));
+    return this;
   }
 
   private void append(StringBuilder build, String key, String value) {
